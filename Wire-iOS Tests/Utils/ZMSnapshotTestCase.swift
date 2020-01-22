0510457694bda7f09a177d14edeaaecff2e48541
bill chan <billypchan@gmail.com>
2020-01-22 11:54:29 +0100

    update snapshots part 2

--- Wire-iOS Tests/Utils/ZMSnapshotTestCase.swift
+++ Wire-iOS Tests/Utils/ZMSnapshotTestCase.swift
@@ -99,8 +99,7 @@ class ZMSnapshotTestCase: FBSnapshotTestCase {
         snapshotBackgroundColor = UIColor.clear
 
         // Enable when the design of the view has changed in order to update the reference snapshots
-//        recordMode = strcmp(getenv("RECORDING_SNAPSHOTS"), "YES") == 0
-        recordMode = true
+        recordMode = strcmp(getenv("RECORDING_SNAPSHOTS"), "YES") == 0
 
         usesDrawViewHierarchyInRect = true
         let contextExpectation: XCTestExpectation = expectation(description: "It should create a context")
